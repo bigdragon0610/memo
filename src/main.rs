cff52d3b83147eefd1e61a06c15706d8fbc519de
ryudai <bigdragon.06102001@gmail.com>
2023-07-13 23:52:54 +0900

    メモの削除が可能

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 use chrono::{DateTime, Local, TimeZone};
 use clap::{Parser, Subcommand};
 use rusqlite::{Connection, Result};
-use std::{
-    fs,
-    io::{Read, Write},
-};
+use std::{fs, io::Write};
 
 /// Simple program to greet a person
 #[derive(Parser)]
@@ -19,12 +16,13 @@ enum Commands {
     Add,
     /// List all memos
     List,
+    /// Delete a memo
+    Delete { id: i32 },
 }
 
 #[derive(Debug)]
 struct Memo {
     id: i32,
-    title: String,
     content: String,
     created_at: DateTime<Local>,
 }
@@ -45,7 +43,6 @@ fn main() -> Result<()> {
     conn.execute(
         "CREATE TABLE IF NOT EXISTS memo (
             id          INTEGER PRIMARY KEY,
-            title       TEXT NOT NULL,
             content     TEXT,
             created_at  TEXT NOT NULL
         )",
@@ -55,6 +52,7 @@ fn main() -> Result<()> {
     match &cli.command {
         Some(Commands::Add) => add_memo(&conn),
         Some(Commands::List) => list_memo(&conn),
+        Some(Commands::Delete { id }) => delete_memo(&conn, *id),
         None => {
             println!("No subcommand was used");
         }
@@ -64,51 +62,52 @@ fn main() -> Result<()> {
 }
 
 fn add_memo(conn: &Connection) {
-    print!("Enter title: ");
+    print!("Enter memo: ");
     std::io::stdout().flush().unwrap();
-
-    let mut title = String::new();
-    std::io::stdin().read_line(&mut title).unwrap();
-    title = title.trim().to_string();
 
     let mut content = String::new();
-    print!("Enter content: ");
-    std::io::stdout().flush().unwrap();
-    std::io::stdin()
-        .lock()
-        .read_to_string(&mut content)
-        .unwrap();
+    std::io::stdin().read_line(&mut content).unwrap();
+    content = content.trim().to_string();
+    if content.is_empty() {
+        println!("Memo is empty");
+        return;
+    }
 
     let memo = Memo {
         id: 0,
-        title,
         content,
         created_at: Local::now(),
     };
     conn.execute(
-        "INSERT INTO memo (title, content, created_at) VALUES (?1, ?2, ?3)",
-        (&memo.title, &memo.content, &memo.created_at.to_rfc3339()),
+        "INSERT INTO memo (content, created_at) VALUES (?1, ?2)",
+        (&memo.content, &memo.created_at.to_rfc3339()),
     )
     .unwrap();
 }
 
 fn list_memo(conn: &Connection) {
     let mut stmt = conn
-        .prepare("SELECT id, title, content, created_at FROM memo")
+        .prepare("SELECT id, content, created_at FROM memo")
         .unwrap();
     let person_iter = stmt
         .query_map([], |row| {
             Ok(Memo {
                 id: row.get(0)?,
-                title: row.get(1)?,
-                content: row.get(2)?,
+                content: row.get(1)?,
                 created_at: Local
-                    .datetime_from_str(&row.get::<_, String>(3)?, "%Y-%m-%dT%H:%M:%S%.f%Z")
+                    .datetime_from_str(&row.get::<_, String>(2)?, "%Y-%m-%dT%H:%M:%S%.f%Z")
                     .unwrap(),
             })
         })
         .unwrap();
     for person in person_iter {
-        println!("Found person {:?}", person.unwrap());
+        if let Ok(person) = person {
+            println!("{}: {}", person.id, person.content);
+        }
     }
+}
+
+fn delete_memo(conn: &Connection, id: i32) {
+    conn.execute("DELETE FROM memo WHERE id = ?1", [id])
+        .unwrap();
 }
